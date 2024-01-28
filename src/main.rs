5c791800b4a939dcc5f21997b99cfcd5b31ed9c5
Ahraman <ahraman12000@gmail.com>
2024-01-28 11:22:58 +0330

    Created basic routing.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
+use axum::{extract::Path, routing::get, Router};
 use dotenvy;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().unwrap();
 
-    println!("Hello world!");
+    let router = build_router();
+
+    let listener = TcpListener::bind("localhost:3000").await.unwrap();
+    axum::serve(listener, router).await.unwrap();
+}
+
+pub fn build_router() -> Router<()> {
+    Router::new()
+        .route("/wiki/", get(root))
+        .route("/wiki/:page", get(get_page))
+}
+
+pub async fn root() -> String {
+    "Hello world!".to_owned()
+}
+
+pub async fn get_page(Path(page): Path<String>) -> String {
+    format!("Hello {page}")
 }
