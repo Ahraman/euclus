b4f4a8a2975b31694e5bb331107361477b348dee
Ahraman <ahraman12000@gmail.com>
2024-01-28 16:28:06 +0330

    Reworked SQL file indentation.

--- migrations/2024-01-28-080101_create_users_table/up.sql
+++ migrations/2024-01-28-080101_create_users_table/up.sql
@@ -1,9 +1,9 @@
 -- Your SQL goes here
 
 CREATE TABLE IF NOT EXISTS users (
-        id              SERIAL PRIMARY KEY,
-        username        VARCHAR(255) NOT NULL UNIQUE,
-        display_name    VARCHAR(255) NOT NULL,
-        pass_word       BYTEA NOT NULL,
-        email           BYTEA NOT NULL
+    id              SERIAL PRIMARY KEY,
+    username        VARCHAR(255) NOT NULL UNIQUE,
+    display_name    VARCHAR(255) NOT NULL,
+    pass_word       BYTEA NOT NULL,
+    email           BYTEA NOT NULL
 );
--- migrations/2024-01-28-105310_create_revisions_table/up.sql
+++ migrations/2024-01-28-105310_create_revisions_table/up.sql
@@ -1,12 +1,12 @@
 -- Your SQL goes here
 
 CREATE TABLE IF NOT EXISTS revisions (
-        id              SERIAL PRIMARY KEY,
-        page_id         INTEGER
-                                REFERENCES pages(id),
-        user_id         INTEGER
-                                REFERENCES users(id),
-        prev_rev        INTEGER DEFAULT NULL
-                                REFERENCES revisions(id),
-        creation_time   TIMESTAMPTZ
+    id              SERIAL PRIMARY KEY,
+    page_id         INTEGER
+                        REFERENCES pages(id),
+    user_id         INTEGER
+                        REFERENCES users(id),
+    prev_rev        INTEGER DEFAULT NULL
+                        REFERENCES revisions(id),
+    creation_time   TIMESTAMPTZ
 );
