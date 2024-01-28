d88c656bc4b80b2042dceeaba4547d89c1e69dab
Ahraman <ahraman12000@gmail.com>
2024-01-28 16:29:45 +0330

    Added deletion behavior for revisions table keys.

--- migrations/2024-01-28-105310_create_revisions_table/up.sql
+++ migrations/2024-01-28-105310_create_revisions_table/up.sql
@@ -3,10 +3,13 @@
 CREATE TABLE IF NOT EXISTS revisions (
     id              SERIAL PRIMARY KEY,
     page_id         INTEGER
-                        REFERENCES pages(id),
+                        REFERENCES pages(id)
+                            ON DELETE RESTRICT,
     user_id         INTEGER
-                        REFERENCES users(id),
+                        REFERENCES users(id)
+                            ON DELETE SET NULL,
     prev_rev        INTEGER DEFAULT NULL
-                        REFERENCES revisions(id),
+                        REFERENCES revisions(id)
+                            ON DELETE CASCADE,
     creation_time   TIMESTAMPTZ
 );
