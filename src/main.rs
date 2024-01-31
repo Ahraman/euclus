e33810fd2df1bd1c29a57ee1ea20ae9d53d015cd
Ahraman <ahraman12000@gmail.com>
2024-01-31 13:15:07 +0330

    Sample code for checking page exists in db.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use axum::{extract::Path, routing::get, Router};
+use diesel::{
+    query_dsl::methods::{FilterDsl, SelectDsl},
+    Connection, ExpressionMethods, PgConnection, RunQueryDsl, SelectableHelper,
+};
 use dotenvy;
 use tokio::net::TcpListener;
+
+use euclus::models::Page;
 
 #[tokio::main]
 async fn main() {
@@ -23,5 +29,23 @@ pub async fn root() -> String {
 }
 
 pub async fn get_page(Path(page): Path<String>) -> String {
-    format!("Hello {page}")
+    use euclus::schema::pages::dsl::*;
+
+    let conn = &mut establish_connection();
+    let res = pages
+        .filter(title.eq(&page))
+        .select(Page::as_select())
+        .first(conn);
+
+    if let Ok(p) = res {
+        format!("Hello {}", p.title)
+    } else {
+        format!("Page '{}' not found!", page)
+    }
+}
+
+fn establish_connection() -> PgConnection {
+    let database_url = std::env::var("DATABASE_URL").unwrap();
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {database_url}"))
 }
